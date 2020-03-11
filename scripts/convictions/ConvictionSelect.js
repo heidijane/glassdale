2b5fbaf5213832e99481883933a6326187bc6b0a
heidijane <heidi@heidisprouse.com>
2020-03-11 14:04:27 -0500

    Added filtering of criminals by crime

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -6,6 +6,24 @@ import { useConvictions } from "./ConvictionProvider.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
+const eventHub = document.querySelector('.container')
+
+// On the content target, listen for a "change" event.
+contentTarget.addEventListener("change", changeEvent => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === 'crimeSelect') {
+        // Create custom event. Provide an appropriate name.
+        const crimeChosenEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                chosenCrime: changeEvent.target.value
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(crimeChosenEvent)
+    }
+})
 
 const ConvictionSelect = () => {
         // Get all convictions from application state
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -2,6 +2,32 @@
   import { Criminal } from "./Criminal.js";
 
   const contentTarget = document.querySelector(".criminalsContainer")
+  const eventHub = document.querySelector('.container')
+
+  eventHub.addEventListener("crimeChosen", event => {
+      //filter the list of criminals who commited the crime  
+
+      //get the criminals
+      const criminals = useCriminals();
+
+      //get the crime
+      const theCrimeThatWasChosen = event.detail.chosenCrime;
+
+      const guiltyCriminals = criminals.filter(criminal => { //criminal holds each individual array element
+          if (criminal.conviction === theCrimeThatWasChosen) {
+              return true
+          }
+          return false
+      })
+
+      //clear the code out before building the list back up again       
+      contentTarget.innerHTML = ""
+
+      //build the list back up again
+      for (const singleCriminal of guiltyCriminals) {
+          contentTarget.innerHTML += Criminal(singleCriminal)
+      }
+  })
 
   export const CriminalList = () => {
       const criminals = useCriminals()
