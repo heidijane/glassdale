cbae547248335ab950eab53c02f380c7aad39774
heidijane <heidi@heidisprouse.com>
2020-03-16 14:48:06 -0500

    Changed the form so that date is filled in automatically

--- scripts/notes/NoteDataProvider.js
+++ scripts/notes/NoteDataProvider.js
@@ -6,17 +6,24 @@ const dispatchStateChangeEvent = () => {
     eventHub.dispatchEvent(noteStateChangedEvent)
 }
 
+let notes = []
+
+export const useNotes = () => {
+    return notes.slice()
+}
+
 const getNotes = () => {
-    fetch('http://localhost:8080/notes')
+    return fetch('http://localhost:8088/notes')
         .then(response => response.json())
         .then(parsedNotes => {
+            //console.table(parsedNotes)
             notes = parsedNotes
         })
 
 }
 
 export const saveNote = note => {
-    fetch('http://localhost:8080/notes', {
+    return fetch('http://localhost:8088/notes', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -4,14 +4,14 @@ const contentTarget = document.querySelector(".noteFormContainer")
 
 const render = () => {
     contentTarget.innerHTML = `
-    <fieldset>
-    <label for="note-date">Date</label>
-        <input type="date" id="note-date">
-        <label for="note-subject">Subject</label>
-        <input type="text" id="note-subject">
-        <label for="note-text">Note</label>
-        <textarea id="note-text" rows="4" cols="50"></textarea>
-        <button id="saveNote">Save Note</button>
+        <fieldset>
+            <label for="note-subject">Subject</label>
+            <input type="text" id="note-subject">
+
+            <label for="note-text">Note</label>
+            <textarea id="note-text" rows="4" cols="50"></textarea>
+
+            <button id="saveNote">Save Note</button>
         </fieldset>
     `
 }
@@ -20,12 +20,15 @@ const render = () => {
 contentTarget.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "saveNote") {
 
+        const subject = document.getElementById("note-subject").value
+        const text = document.getElementById("note-text").value
+
         //Make a new object representation of a note
         const newNote = {
             // Key/value pairs here
-            date: document.getElementById("note-date").value,
-            subject: document.getElementById("note-subject").value,
-            text: document.getElementById("note-text").value
+            date: Date.now(),
+            subject: subject,
+            text: text
         }
 
         // Change API state and application state
