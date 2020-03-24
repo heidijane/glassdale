26f3ebea8e23d41e70cd2263fc4dc738dcb2535e
heidijane <heidi@heidisprouse.com>
2020-03-24 15:33:52 -0500

    Criminals are now sorted by last name

--- scripts/criminals/CriminalProvider.js
+++ scripts/criminals/CriminalProvider.js
@@ -1,6 +1,19 @@
 let criminals = [];
 
 export const useCriminals = () => {
+    // Sort by criminal last name before creating the copy of the array
+    criminals.sort(
+        (currentThing, nextThing) => {
+            // Get the last name on both objects
+            const [currFirstName, currLastName] = currentThing.name.split(" ")
+            const [nextFirstName, nextLastName] = nextThing.name.split(" ")
+
+            if (currLastName < nextLastName) { return -1; }
+            if (currLastName > nextLastName) { return 1; }
+            return 0;
+        }
+    )
+
     return criminals.slice()
 }
 
